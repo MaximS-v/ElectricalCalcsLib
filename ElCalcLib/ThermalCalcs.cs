d954dffb7e719ab6ef4c61164364c678dd5f9382
Max Safronov <bmc938@gmail.com>
2022-08-24 16:20:02 +0300

    Removed

--- ElCalcLib/ThermalCalcs.cs
+++ ElCalcLib/ThermalCalcs.cs
@@ -51,17 +51,6 @@
         };
 
         /// <summary>
-        /// Heat transfer coefficient for electrical cabinets in various materials, W/m2/deg_cels
-        /// </summary>
-        //public struct HeatTransferCoeff
-        //{
-        //    public const double MetallicPainted = 5.5;
-        //    public const double Polyester = 3.5;
-        //    public const double StainlessSteel = 3.7;
-        //    public const double Aluminum = 12;
-        //}
-
-        /// <summary>
         /// Calculates excess heat output of electrical cabinet
         /// </summary>
         /// <param name="componentsPower">Power dissipated by internal components</param>
@@ -71,7 +60,6 @@
         /// <param name="outTmax">Maximum possible temperature outside the cabinet</param>
         /// <returns>The amount of heat power that needs to be removed or Double.MaxValue if outTmax >= inTmax</returns>
         public static double ExcessHeatOutput(double componentsPower, double heatTransferCoeff,
-
             double effectiveHeatExchangeArea, double inTmax, double outTmax)
         {
             if (outTmax >= inTmax) return Double.MaxValue;
